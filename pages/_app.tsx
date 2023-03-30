99e6cebd09bd9fb23dd8848522bc4d8d317939d1
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2023-03-30 14:26:38 -0400

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from "next/app";
 import Navbar from "../components/navbar";
 import BallsBackground from "../components/bouncing-balls";
 import BallsInteractiveBackground from "../components/bouncing-balls-interactive";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Inter({ subsets: ["latin"] });
 
@@ -11,8 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={font.className}>
       <Navbar>
-      <BallsBackground/>
-      <Component {...pageProps} />
+        <BallsBackground />
+        <Component {...pageProps} />
+        <Analytics />
       </Navbar>
     </main>
   );
