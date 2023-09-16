911b242ce12ab40b5b2e6a0896f0eb41013636d2
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2023-09-16 15:50:48 -0600

    icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <link rel="shortcut icon" href="/static/favicon.ico" />
       <body>
         <Main />
         <NextScript />
