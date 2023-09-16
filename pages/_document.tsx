8bd1ea31594f9c033af8aaf70de8232f5201bafe
OliverMoscow <56978722+OliverMoscow@users.noreply.github.com>
2023-09-16 15:55:01 -0600

    title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <link rel="shortcut icon" href="/static/favicon.ico" />
+      <title>Oliver Moscow's Website</title>
       <body>
         <Main />
         <NextScript />
